963992cd0db0a24b8223b9bc8b51cfa188990fc2
nilevaishnavi <nilesvaishnavi@gmail.com>
2026-09-09 11:06:34 +0530

    Customer address

--- food-delivery-backend/src/main/java/com/ait/app/repository/AdressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ait.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ait.app.model.Adress;
-
-public interface AdressRepository extends JpaRepository<Adress, Integer> {
-
-}
