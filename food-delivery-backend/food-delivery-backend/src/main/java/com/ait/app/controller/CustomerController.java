74d9435b4a969cb71c9a76305d9daad3f87533d0
Swati Gawande <swatigawande825@gmail.com>
2026-09-09 23:20:06 +0530

    View Customer Profile Created.

--- food-delivery-backend/food-delivery-backend/src/main/java/com/ait/app/controller/CustomerController.java
+++ food-delivery-backend/food-delivery-backend/src/main/java/com/ait/app/controller/CustomerController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ait.app.dto.CustomerProfileResponseDto;
 import com.ait.app.model.Customer;
 import com.ait.app.repository.CustomerRepository;
 import com.ait.app.service.CustomerService;
@@ -36,6 +38,14 @@ public class CustomerController {
         List<Customer> list =customerService.getAllCustomers();
 
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerProfileResponseDto> getCustomer(@PathVariable int id)
+    {
+    	CustomerProfileResponseDto responce=customerService.getCustomer(id);
+    	
+    	return new ResponseEntity<CustomerProfileResponseDto>(responce, HttpStatus.OK);
     }
 }
 
