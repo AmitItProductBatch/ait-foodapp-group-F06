98f052f53b35baa21060479e93daad153313494c
Inci1888 <satoru.gojo1888@gmail.com>
2026-09-11 18:17:08 +0530

    ODF-109
    OFD-109 View Restaurent is added

--- food-delivery-backend/src/main/java/com/ait/app/controller/RestaurantController.java
+++ food-delivery-backend/src/main/java/com/ait/app/controller/RestaurantController.java
@@ -1,14 +1,18 @@
 package com.ait.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ait.app.dto.RestaurantDto;
+import com.ait.app.model.Restaurant;
 import com.ait.app.service.RestaurantService;
 
 @RestController
@@ -21,4 +25,13 @@ public class RestaurantController {
 		RestaurantDto dto = restaurantService.addRestaurant(restaurantDto);
 		return new ResponseEntity(dto, HttpStatus.CREATED);
 	}
+	
+	@GetMapping("viewRestaurant")
+	ResponseEntity<List<Restaurant>> GetAllRestaurant()
+	{
+		List<Restaurant> restaurant = restaurantService.GetAllRestaurant();
+		return new ResponseEntity<List<Restaurant>>(restaurant, HttpStatus.OK);
+	}
+	
 }
+
