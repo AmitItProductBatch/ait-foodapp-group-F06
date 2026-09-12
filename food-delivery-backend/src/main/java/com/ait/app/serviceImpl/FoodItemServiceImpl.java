59197c2ed4c5f8b5c31f61f637e6e8eb952f9bd6
Vikesh Namdev <vikesh.namdev.dev@gmail.com>
2026-09-12 22:30:50 +0530

    OFD-113 Update FoodItem created

--- food-delivery-backend/src/main/java/com/ait/app/serviceImpl/FoodItemServiceImpl.java
+++ food-delivery-backend/src/main/java/com/ait/app/serviceImpl/FoodItemServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ait.app.serviceImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.ait.app.dto.FoodItemDto;
 import com.ait.app.exception.FoodItemException;
+import com.ait.app.exception.UpdateCustomerProfileException;
 import com.ait.app.model.FoodItem;
 import com.ait.app.model.Restaurant;
 import com.ait.app.repository.FoodItemRepository;
@@ -28,7 +30,7 @@ public class FoodItemServiceImpl implements FoodItemService {
 
 		Restaurant restaurant = restaurantRepository.findById(dto.getRestaurantId()).get();
 		if (restaurant == null) {
-		    throw new FoodItemException("Restaurant not found", HttpStatus.NOT_FOUND);
+			throw new FoodItemException("Restaurant not found", HttpStatus.NOT_FOUND);
 		}
 
 		FoodItem foodItem = new FoodItem();
@@ -59,5 +61,30 @@ public class FoodItemServiceImpl implements FoodItemService {
 	public List<FoodItem> getAllFoodItems(int restaurantId) {
 		List<FoodItem> list = foodItemRepository.findAllFoodItemByRestaurantId(restaurantId);
 		return list;
+	}
+
+	@Override
+	public FoodItemDto updateFoodItem(int id, FoodItemDto foodItemDto) {
+
+		FoodItem existingFoodItem = foodItemRepository.findById(id)
+				.orElseThrow(() -> new FoodItemException("FoodItem not found with id: " + id, HttpStatus.NOT_FOUND));
+
+		existingFoodItem.setName(foodItemDto.getName());
+		existingFoodItem.setDescription(foodItemDto.getDescription());
+		existingFoodItem.setPrice(foodItemDto.getPrice());
+		existingFoodItem.setAvailability(foodItemDto.getAvailability());
+		existingFoodItem.setCategory(foodItemDto.getCategory());
+
+		FoodItem foodItem = foodItemRepository.save(existingFoodItem);
+
+		FoodItemDto foodItemResponse = new FoodItemDto();
+
+		foodItemResponse.setName(foodItem.getName());
+		foodItemResponse.setDescription(foodItem.getDescription());
+		foodItemResponse.setPrice(foodItem.getPrice());
+		foodItemResponse.setAvailability(foodItem.getAvailability());
+		foodItemResponse.setCategory(foodItem.getCategory());
+
+		return foodItemResponse;
 	}
 }
